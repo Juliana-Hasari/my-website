4d13596911c0d3510e9368628b8621213a669d79
Juliana-Hasari <oppojuliana32@gmail.com>
2025-02-02 02:09:23 +0700

    Initial commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,9 @@
-// src/app/not-found.js
 export default function NotFound() {
-  return (
-    <div>
-      <h1>404: Halaman Tidak Ditemukan</h1>
-      <p>Halaman yang Anda cari tidak tersedia.</p>
-    </div>
-  );
-}
+    return (
+      <div>
+        <h1>404: Halaman Tidak Ditemukan</h1>
+        <p>Halaman yang Anda cari tidak tersedia.</p>
+      </div>
+    );
+  }
+  
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  output: "standalone", // Pastikan ini ada
 };
 
 export default nextConfig;
+
+// const nextConfig = {
+//   output: "export", // Ubah agar Next.js bisa diekspor statis
+//   experimental: {
+//     appDir: true, // Pastikan ini aktif jika pakai App Router
+//   },
+// };
+
+// export default nextConfig;
+
