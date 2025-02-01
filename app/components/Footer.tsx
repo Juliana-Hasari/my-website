ac13d3e88f6c4d03a92527858d9e3a8627beffe0
Juliana-Hasari <oppojuliana32@gmail.com>
2025-02-02 03:53:14 +0700

    Initial commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   console.log("Rendering Footer Component");
   return (
     <footer className="bg-green-700 text-white py-4">
-      <div className="container mx-auto px-4 md:px-0 space-y-6 md:space-y-0 text-justify text-center">
+      <div className="container mx-auto text-center px-4 md:px-0">
         <p>&copy; 2023 Gampong Tanjong Hagu. Hak Cipta Dilindungi.</p>
       </div>
     </footer>
--- app/components/Kontak.tsx
+++ app/components/Kontak.tsx
@@ -2,17 +2,17 @@ export default function Kontak() {
   console.log("Rendering Kontak Component");
   return (
     <section id="kontak" className="bg-green-500 text-white py-20">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-4 md:px-8">
         <h2 className="text-3xl font-bold mb-8 text-center">Hubungi Kami</h2>
-        <div className="flex justify-around">
-          <div>
+        <div className="flex flex-col md:flex-row justify-around space-y-8 md:space-y-0 md:space-x-8">
+          <div className="text-justify">
             <h3 className="text-xl font-semibold mb-4">Alamat</h3>
             <p>Gampong Tanjong Hagu</p>
             <p>Kecamatan Peukan Baro, Kabupaten Pidie</p>
             <p>Provinsi Aceh, Indonesia</p>
             <p>Kode Pos: 24172</p>
           </div>
-          <div>
+          <div className="text-justify">
             <h3 className="text-xl font-semibold mb-4">Kontak</h3>
             <p>Telepon: (0821)4502-5877</p>
             <p>Geucik: Maizar</p>
