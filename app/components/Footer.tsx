1ff016347eb3700567582926c11e28c2aaf0e4ae
Juliana-Hasari <oppojuliana32@gmail.com>
2025-02-02 03:43:07 +0700

    Initial commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,10 @@
 export default function Footer() {
   console.log("Rendering Footer Component");
-    return (
-      <footer className="bg-green-700 text-white py-4">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2023 Gampong Tanjong Hagu. Hak Cipta Dilindungi.</p>
-        </div>
-      </footer>
-    )
-  }
-  
-  
+  return (
+    <footer className="bg-green-700 text-white py-4">
+      <div className="container mx-auto px-4 md:px-0 space-y-6 md:space-y-0 text-justify text-center">
+        <p>&copy; 2023 Gampong Tanjong Hagu. Hak Cipta Dilindungi.</p>
+      </div>
+    </footer>
+  );
+}
