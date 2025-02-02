d65ffb37bb786009ff8edbddb92e7ada6fe0f79c
Juliana-Hasari <oppojuliana32@gmail.com>
2025-02-02 08:19:40 +0700

    Initial commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-green-700 text-white py-4">
       <div className="container mx-auto text-center px-4 md:px-0">
-        <p>&copy; 2023 Gampong Tanjong Hagu. Hak Cipta Dilindungi.</p>
+        <p>&copy; 2025 Gampong Tanjong Hagu. Hak Cipta Dilindungi.</p>
       </div>
     </footer>
   );
